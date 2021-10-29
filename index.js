e30cd5c26df4adf4558ab5245949786f7ceecf34
JarnoVoogd <jarno.voogd@hva.nl>
2021-10-29 12:30:15 +0200

    Rewriting annotations, Dutch > English

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
 
-// data.json bestand ophalen
+// requiring data.json file
 const data = require('./data.json');
 
-// Arrays aanmaken om de data in te zetten
+// Creating arrays to store the data in 
 let huisdierData = [];
 let favoDatumData = [];
 let correcteData = [];
 let somPerSoort = [];
 
 
-// For loop die elk array in het data.json bestand langs gaat
-    // Specefiek zoekt naar 'favourite-pet' en die data in het huisdierData array zet
-    // Het schoont de data op, het geeft elk woord een hoofdletter
-    //Tot slot maakt het gebruik van de functie twoSpaces, uitleg bij de betreffende functie
+// For loop that runs through every array in the  data.json file
+// The loop specifically looks for 'favourite-pet' and puts that data in the huisdierData array
+// It cleans up the data bij capitalizing each (first) word
+// Lastly it uses the function twoSpaces, more details in the annotations above twoSpaces
 for (let i = 0; i < data.length; i++) {
     huisdierData.push(twoSpaces(data[i]['favourite-pet']));
     huisdierData[i] = huisdierData[i]
@@ -27,15 +27,13 @@ for (let i = 0; i < data.length; i++) {
     favoDatumData.push(data[i]['favourite-date']);
     favoDatumData[i] = favoDatumData[i]
         .split("/")
-
-
 } 
 
 
-// Deze functie checkt elke string op spaties
-    // Bij elke spatie splitst hij de string op
-    // Bij meer dan 2 resulterende strings verwijderd hij alles behalve de eerste string
-    // Dit garandeerd dat dierennamen met twee delen blijven staan maar alle input met ' of ' erin na het eerste deel verwijderd worden
+// This function checks every string for spaces
+// At every encounter with a space it splits up the string
+// After splitting the string at every space it checks if  the original string consists of more than two strings, if it does it deletes everything except the first resulting string
+// This guarantees that animal names consisting of two words remain but all input with ' of ' will be deleted after the first word ends
 function twoSpaces(string){
     let array = string.split(" ");
     if (array.length > 2) {
@@ -44,19 +42,15 @@ function twoSpaces(string){
  return string
 }
 
-
-
-
-
-// Hier ga ik elke string in huisdierData langs en telt ze per soort op
-    // Is een soort string nog niet voorbijgekomen is het aantal dus 0, komt de string wel voorbij wordt +1 gedaan
+// This line of code runs through every string in huisdierData and adds up the amount of occurences per 'animal'
+// If a certain type of animal hasn't occured yet the amount of occurences stays 0, when that type of animal does occur it adds 1 to that category 
 huisdierData.forEach((i) =>{ somPerSoort[i] = (somPerSoort[i]||0) + 1;});
 
 
 
 
-// Dit is een functie waarin elk array met een datum erin wordt nagelopen en omgezet naar romeinse cijfers.
-// Door gebruik te maken van een forEach kan dit een stuk simpeler dan met een for loop
+// This function runs through every array, containing a date, and converts the date from arabic to roman numerals 
+// By using forEach this can be achieved with less code than if I were to use a For-Loop 
 favoDatumData.forEach((datum) =>{ 
     datum[0]=convertToRoman(datum[0]);
     datum[1]=convertToRoman(datum[1]);
@@ -64,13 +58,15 @@ favoDatumData.forEach((datum) =>{
     favoDatumData.join('-');
 });
 
-
+// This For-Loop runs through all new, to roman converted, strings in the favoDatumData array
+// It then pushes all converted dates to a new array, correcteData
+// In the proces of pushing it also runs the function rejoin
 for (let i = 0; i < favoDatumData.length; i++) {
     correcteData.push(rejoin(favoDatumData[i]));
 }
 
-
-
+// This function takes the input array and rejoins the strings in the array
+// It rejoins the strings with a '-' in between
 function rejoin(string){
     let array = string.join('-');
     return array;
@@ -80,9 +76,9 @@ function rejoin(string){
 
 
 
-// Ik heb hier een functie van internet gehaald, link hieronder
+// This funcion was taken from stackoverflow, link below
 // https://stackoverflow.com/questions/9083037/convert-a-number-into-a-roman-numeral-in-javascript
-// Deze functie zet arabische getallen om in romeinse getallen.
+// This function converts arabic numerals to roman
 function convertToRoman(num) {
     //create key:value pairs
     var romanLookup = {M:1000, D:500, C:100, L:50, X:10, V:5, I:1};
@@ -136,22 +132,27 @@ function convertToRoman(num) {
   }
   
 
-// Hier maak ik een variabele aan, hierin laat ik mijn array, correcteData
-    // omzetten naar 
-let testString = correcteData.toString()
+// Here I created a variable in which convert my array, correcteData, from an array containing strings to one long string
+// The reason i did this is so i can more easily count the amount of a certain character.
+// I could've used a for loop to run through every string seperately but that would've been a lot of (unnecessary) work
+let datumString = correcteData.toString()
 
+// Here I created an object in which I count the amount of occurences of a certain character
+// The way I do this is by replacing every chacacter in the string, that isn't my desired character, with 'nothing'
+// This way the only characters remaining in the string are the characters I want to count the occurences of
+// The amount of occurences of the character is equal to the length of the (remaining) string
 let aantalKeer = {
-    M: testString.replaceAll(/[^M]/g, "").length,
-    C: testString.replaceAll(/[^C]/g, "").length,
-    X: testString.replaceAll(/[^X]/g, "").length,
-    V: testString.replaceAll(/[^V]/g, "").length,
-    I: testString.replaceAll(/[^I]/g, "").length
+    M: datumString.replaceAll(/[^M]/g, "").length,
+    C: datumString.replaceAll(/[^C]/g, "").length,
+    X: datumString.replaceAll(/[^X]/g, "").length,
+    V: datumString.replaceAll(/[^V]/g, "").length,
+    I: datumString.replaceAll(/[^I]/g, "").length
 }
 
 
 
 
-// Hier roep ik de opgeslagen data in de arrays op
+// Here I console.log the results of my code
 console.log(huisdierData);
 console.log(somPerSoort);
 console.log(correcteData)
