d77e0dbe2f0fc9a610f1e939fbb95de01f83ca8e
JarnoVoogd <jarno.voogd@hva.nl>
2021-11-04 16:04:14 +0100

    refactor: edited my for loop completely

--- Week2/index.js
+++ Week2/index.js
@@ -4,19 +4,24 @@ import fetch from "node-fetch";
 let array = [];
 
 // This function fetches data from SWAPI and seperates the .json part
-function test(id) {
+function fetchPeople(id) {
   const url = `https://swapi.dev/api/people/${id}/}`;
   return fetch(url)
     .then(response => response.json())
     .then(data => data);
 }
 
-// This for loop loops through the data fetched by 'test' and pushes it to  my array
-// The data is put in an object so 'array' will be an array with objects
-for (let i = 1; i < 6; i++) {
-  let person = await test(i);
-  array.push({ name: person.name, vehicles: person.vehicles });
+// Variable I use to count the amount of errors in the data
+// This was necessary because of 0 < 5 amount of errors in the API data
+let endFetch = 0;
+
+// for loop that~
+for (let i = 0; endFetch < 5; i++) {
+  await fetchPeople(i).then(persoon => {
+    if (!persoon.detail) {
+      allFetches.push(persoon);
+    } else {
+      endFetch++;
+    }
+  });
 }
-
-console.log(array);
-
