851554c057f1c15404a9ecd5083b758c7f24bf85
JarnoVoogd <jarno.voogd@hva.nl>
2021-11-04 16:01:43 +0100

    feat: notations added to first draft of code

--- Week2/index.js
+++ Week2/index.js
@@ -1,7 +1,9 @@
 import fetch from "node-fetch";
 
+// Creating an array to push data to 
 let array = [];
 
+// This function fetches data from SWAPI and seperates the .json part
 function test(id) {
   const url = `https://swapi.dev/api/people/${id}/}`;
   return fetch(url)
@@ -9,9 +11,12 @@ function test(id) {
     .then(data => data);
 }
 
+// This for loop loops through the data fetched by 'test' and pushes it to  my array
+// The data is put in an object so 'array' will be an array with objects
 for (let i = 1; i < 6; i++) {
   let person = await test(i);
   array.push({ name: person.name, vehicles: person.vehicles });
 }
 
 console.log(array);
+
